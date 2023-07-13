a431c9f183658bdbb940734b05cee77379a1f496
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-07-13 09:00:25 +0200

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sample1';
+  consoleOutput: string = '';
+
+  ngOnInit() {
+    const myObservable = of(1, 2, 3);
+
+    // Create observer object
+    const myObserver = {
+      next: (x: number) => {
+        this.consoleOutput += 'Observer got a next value: ' + x + '<br>';
+      },
+      error: (err: Error) => {
+        this.consoleOutput += 'Observer got an error: ' + err + '<br>';
+      },
+      complete: () => {
+        this.consoleOutput += 'Observer got a complete notification' + '<br>';
+      },
+    };
+
+    // Execute with the observer object
+    myObservable.subscribe(myObserver);
+  }
 }
